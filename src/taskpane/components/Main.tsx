59d29ee6ee9f057eab8774c8c563f44264ab5127
montheramer <monther.amer48@gmail.com>
2021-03-03 21:58:43 +0200

    read values from excel sheet

--- src/taskpane/components/Main.tsx
+++ src/taskpane/components/Main.tsx
@@ -2,25 +2,33 @@ import * as React from "react";
 import { Button, ButtonType } from "office-ui-fabric-react";
 
 const Component = () => {
-  const [state, setState] = React.useState({ value: "" });
+  const [state, setState] = React.useState({ cells: [], error: "" });
+
+  const handleCopy = async () => {
+    try {
+      await Excel.run(async context => {
+        const range = context.workbook.getSelectedRange();
+
+        range.load("address");
+        range.load("values");
+
+        await context.sync();
+
+        setState({ ...state, cells: range.values });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="container">
-      <p>{state.value}</p>
-      {state.value.length ? <p>{state.value} </p> : null}
-      <Button
-        className="button"
-        buttonType={ButtonType.primary}
-        onClick={() => setState({ ...state, value: "monther" })}
-      >
+      <p>{state.cells}</p>
+      <Button className="button" buttonType={ButtonType.primary} onClick={handleCopy}>
         COPY
       </Button>
 
-      <Button
-        className="button"
-        buttonType={ButtonType.primary}
-        onClick={() => setState({ ...state, value: "monther" })}
-      >
+      <Button className="button" buttonType={ButtonType.primary} onClick={() => setState({ ...state })}>
         PASTE
       </Button>
     </div>
