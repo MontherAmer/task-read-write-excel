d54b0105931a9efbbd7595545ee2cca75c83af31
montheramer <monther.amer48@gmail.com>
2021-03-03 22:11:39 +0200

    write to sheet

--- src/taskpane/components/Main.tsx
+++ src/taskpane/components/Main.tsx
@@ -14,21 +14,46 @@ const Component = () => {
 
         await context.sync();
 
-        setState({ ...state, cells: range.values });
+        setState({ ...state, cells: range.values, error: "" });
       });
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handlePaste = async () => {
+    try {
+      if (!state.cells.length) {
+        setState({ ...state, error: "You should copy data first" });
+      } else {
+        await Excel.run(async context => {
+          const range = context.workbook.getSelectedRange();
+          range.load("address");
+          range.load("values");
+          await context.sync();
+
+          let rows = state.cells.length;
+          let cols = state.cells[0].length;
+          let range2 = range.getAbsoluteResizedRange(rows, cols);
+
+          range2.values = state.cells;
+          await context.sync();
+        });
+      }
+    } catch (error) {
+      setState({ ...state, error: JSON.stringify(error) });
+      console.error(error);
+    }
+  };
+
   return (
     <div className="container">
-      <p>{state.cells}</p>
+      {state.error.length ? <p className="error">{state.error} </p> : null}
       <Button className="button" buttonType={ButtonType.primary} onClick={handleCopy}>
         COPY
       </Button>
 
-      <Button className="button" buttonType={ButtonType.primary} onClick={() => setState({ ...state })}>
+      <Button className="button" buttonType={ButtonType.primary} onClick={handlePaste}>
         PASTE
       </Button>
     </div>
